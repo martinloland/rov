93b524b36b078f7f434eaaf86d1f3918f5a2c8d3
martinloland <martinloland@gmail.com>
2016-11-03 21:42:07 +0100

    Update loopFunctions.ino

--- arduino/loopFunctions.ino
+++ arduino/loopFunctions.ino
@@ -5,45 +5,78 @@ SENS readSensors() {
   temp.pressure = 3024;
   temp.temp = 18.3;
   temp.volt = 10.8;
+  temp.curr = 2.12;
   temp.gyr.ax = 0;
   temp.gyr.ay = 0;
   temp.gyr.az = 0;
   temp.gyr.compass = 0;
-
   temp = mpuLoop(temp); // read orientation
 
   return  temp;
 }
 
-ACT communicate(SENS sens) {
-  ACT temp;
-  if (Serial.available() > 0) {
-    //Receive data
-    String incoming = Serial.readString();
+ACT communicate(SENS sens, ACT temp) {
 
-    // Send data back
-    String sending = createString(sens);
-    Serial.println(sending);
+  char inData[100]; // Allocate some space for the string
+  char inChar; // Where to store the character read
+  byte index = 0; // Index into array; where to store the character
+  if (Serial.available() > 0) {
+    // Read in a whole string
+    while (Serial.available() > 0) {
+      inChar = Serial.read(); // Read a character
+      inData[index] = inChar; // Store it
+      index++; // Increment where to write next
+      inData[index] = '\0'; // Null terminate the string
+    }
+    // Update sensors
+    temp = stringToClass(inData);
   }
 
-  return  temp;
+  // Send actuator settings as string
+  Serial.println(classToString(sens));
+
+  return temp;
 }
 
-ACT communicateProto(SENS sens) {
+ACT stringToClass(char *inData) {
+  char *curr;
+  char *next;
+  char *att;
+  int value;
   ACT temp;
-  if (Serial.available() > 0) {
-    //Receive data
-    String incoming = Serial.readString();
+
+  next = strstr(inData, ",") + 1;
+
+  while (next - 1 != NULL) {
+    curr = next;
+    // Prepare next iteration
+    next = strstr(next, ",") + 1;
+    // Insert NULL character at delim
+    strcpy(next - 1, "\0");
+    // Update the corrosponding attribute
+    att = strtok(curr, ":");
+    value = atol(strtok(NULL, ":"));
+    if (strcmp(att, "led") == 0) {
+      temp.led = value;
+    } else if (strcmp(att, "pan") == 0) {
+      temp.pan = value;
+    } else if (strcmp(att, "tilt") == 0) {
+      temp.tilt = value;
+    } else if (strcmp(att, "lf") == 0) {
+      temp.mot.lf = value;
+    } else if (strcmp(att, "rf") == 0) {
+      temp.mot.rf = value;
+    } else if (strcmp(att, "lb") == 0) {
+      temp.mot.lb = value;
+    } else if (strcmp(att, "rb") == 0) {
+      temp.mot.rb = value;
+    }
   }
 
-  // Send data back
-  String sendString = createString(sens);
-  Serial.println(sendString);
-
-  return  temp;
+  return temp;
 }
 
-String createString(SENS sens) {
+String classToString(SENS sens) {
   String str = "SENSORS,";
   str += "press:";
   str += sens.pressure;
@@ -51,6 +84,8 @@ String createString(SENS sens) {
   str += sens.temp;
   str += ",volt:";
   str += sens.volt;
+  str += ",curr:";
+  str += sens.curr;
   str += ",roll:";
   str += sens.gyr.roll;
   str += ",yaw:";
@@ -70,5 +105,19 @@ String createString(SENS sens) {
 }
 
 void moveActuator(ACT act) {
+  led(act);
+  panTilt(act);
+}
 
+void panTilt(ACT act) {
+  panServo.write(act.pan);
+  //tiltServo.write(act.tilt);
+}
+
+void led(ACT act) {
+  if (act.led == 1) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
