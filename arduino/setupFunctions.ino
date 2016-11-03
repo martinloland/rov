13df253803979e8a38f3f2ad4331faedc7f1d304
martinloland <martinloland@gmail.com>
2016-11-03 21:43:54 +0100

    Update setupFunctions.ino

--- arduino/setupFunctions.ino
+++ arduino/setupFunctions.ino
@@ -1,9 +1,10 @@
-void startPins(){
-	
+void startPins() {
+  pinMode(PAN_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void initialize(){
-	//panServo.attach(panPin);
-	//panServo.attach(tiltPin);
-	mpuSetup();
+void initialize() {
+  panServo.attach(PAN_PIN);
+  //panServo.attach(tiltPin);
+  mpuSetup();
 }
