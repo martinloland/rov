7175dd1e2cf4be0ef7a3b8d8b142d1c0f854a0c5
martinloland <martinloland@gmail.com>
2016-11-03 21:43:30 +0100

    Update rov.ino

--- arduino/rov.ino
+++ arduino/rov.ino
@@ -1,24 +1,22 @@
-#include "arduinoRov.h"
+#include "rov.h" // Main header file
 #include "mpu01.h" // MPU6050 header file
 #include <Servo.h>
 #include <Wire.h> //Gyroscope
 Servo panServo;
 Servo tiltServo;
 
-int temp;
-
 // Create structures that hold sensor values and actuator positions
 SENS sens;
 ACT act;
 
-void setup(){
-	Serial.begin (115200); //9600 115200
-	startPins();
-	initialize();
+void setup() {
+  Serial.begin (115200); //9600 115200
+  startPins();
+  initialize();
 }
 
-void loop(){
-	sens = readSensors(); // Read sensors values
-	act = communicateProto(sens); // Send sensor values and receive actuator values
-	moveActuator(act); // Update position of actuators
+void loop() {
+  sens = readSensors(); // Read sensors values
+  act = communicate(sens, act); // Send sensor values and receive actuator values if new
+  moveActuator(act); // Update position of actuators
 }
